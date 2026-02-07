8c223275af406a48baeac66ac680a0a80d478be9
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-07 14:23:13 -0500

    added user org data

---
+++ api/src/app/user-organizations/user-organization.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  Column,
+} from 'typeorm';
+import { User } from '../users/user.entity';
+import { Organization } from '../organizations/organization.entity';
+import { Role } from './role.enum';
+
+@Entity()
+export class UserOrganization {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => User, (user) => user.memberships)
+  user!: User;
+
+  @ManyToOne(() => Organization)
+  organization!: Organization;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+  })
+  role!: Role;
+}
