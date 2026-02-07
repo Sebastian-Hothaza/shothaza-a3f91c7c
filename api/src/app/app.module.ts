09fb8436b5ad2034f787eaddfd74392efc6e23ad
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-07 13:54:28 -0500

    Added postgres connection

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // Connect to PostgreSQL database using TypeORM
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nx_demo',
+      autoLoadEntities: true,
+      synchronize: true, // dev only
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
