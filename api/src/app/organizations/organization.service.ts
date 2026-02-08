f96d076b16437edafdd08b727507ed4499423415
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-08 14:40:58 -0500

    add org service

---
+++ api/src/app/organizations/organization.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Organization } from '../organizations/organization.entity';
+
+@Injectable()
+export class OrganizationService {
+    constructor(
+        @InjectRepository(Organization)
+        private orgRepo: Repository<Organization>,
+    ) { }
+
+    // Returns the id of the provided org as well as the id's of parent orgs
+    async getOrgAndAncestors(orgId: number): Promise<number[]> {
+        const ids: number[] = [];
+        let current = await this.orgRepo.findOne({ where: { id: orgId }, relations: ['parent'], }); // We have the relations 'parent' to prevent lazy loading
+
+        while (current) {
+            ids.push(current.id);
+            if (!current.parent) break;
+            current = await this.orgRepo.findOne({ where: { id: current.parent.id } });
+        }
+        return ids;
+    }
+}
