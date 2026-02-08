17002dbdd2816fde3468ac0bf0a77abfdee927e1
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-07 21:55:20 -0500

    added cookie parser

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,12 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
   const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(
