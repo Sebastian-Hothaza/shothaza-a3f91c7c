2d4d9c1816e0d399dcc4710f81c4ea1066c040b7
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-08 12:06:20 -0500

    Revised task create to include org

--- api/src/app/tasks/tasks.module.ts
+++ api/src/app/tasks/tasks.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { Task } from './task.entity';
+import { UserOrganization } from '../user-organizations/user-organization.entity';
+import { Organization } from '../organizations/organization.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), TypeOrmModule.forFeature([UserOrganization]), TypeOrmModule.forFeature([Organization])],
   providers: [TasksService],
   controllers: [TasksController],
 })
-export class TasksModule {}
+export class TasksModule { }
--- api/src/app/tasks/tasks.service.ts
+++ api/src/app/tasks/tasks.service.ts
@@ -2,16 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
+import { Organization } from '../organizations/organization.entity';
 
 @Injectable()
 export class TasksService {
     constructor(
         @InjectRepository(Task)
         private repo: Repository<Task>,
+        @InjectRepository(Organization)
+        private orgRepo: Repository<Organization>,
     ) { }
 
-    create(title: string, category: string) {
-        const task = this.repo.create({ title, category });
+    async create(title: string, category: string, organizationId: number) {
+        const org = await this.orgRepo.findOne({ where: { id: organizationId } });
+        if (!org) throw new Error('Organization not found');
+
+        const task = this.repo.create({
+            title,
+            category,
+            organization: org, // <-- assign the entity, not the ID
+        });
+
         return this.repo.save(task);
     }
 
