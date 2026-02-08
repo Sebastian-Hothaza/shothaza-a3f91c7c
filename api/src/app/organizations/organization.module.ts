d9cd17171cff0efd85fcbfd8cdab1b30a9b8ddcb
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-08 14:40:51 -0500

    provided org service

--- api/src/app/organizations/organization.module.ts
+++ api/src/app/organizations/organization.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Organization } from './organization.entity';
+import { OrganizationService } from './organization.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Organization])],
+  providers: [OrganizationService],
 })
-export class OrganizationsModule {}
+export class OrganizationsModule { }
 
