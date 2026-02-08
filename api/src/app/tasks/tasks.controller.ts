9e34bf0bd7f6f7d18ad25a9da0e349194307c3d2
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-08 12:05:56 -0500

    Added jwt protection and revised task create to include org

--- api/src/app/tasks/tasks.controller.ts
+++ api/src/app/tasks/tasks.controller.ts
@@ -3,36 +3,36 @@ import { TasksService } from './tasks.service';
 import { UpdateTaskDto } from './task-update.dto';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RbacGuard } from '../auth/rbac.guard';
+import { Roles } from '../auth/roles.decorator';
+import { Role } from '../user-organizations/role.enum';
 
-
+@UseGuards(JwtAuthGuard, RbacGuard) // As per assignment spec, ALL endpoints must invlude token verification
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly tasksService: TasksService) { }
 
+    // Only admins & owners can create tasks. Task's org MUST match users org OR in case where user is member of parent company, allow generate in child company
     @Post()
-    create(@Body('title') title: string, @Body('category') category: string) {
-        return this.tasksService.create(title, category);
+    create(@Body('title') title: string, @Body('category') category: string, @Body('organizationId') organizationId: number) {
+        return this.tasksService.create(title, category, organizationId);
     }
 
-    @UseGuards(JwtAuthGuard)
+    // List tasks, if in parent company, then list those in child company
+    @Roles(Role.ADMIN)
     @Get()
     findAll() {
         return this.tasksService.findAll();
     }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.tasksService.findOne(Number(id));
-    }
 
-
+    // Same as create
     @Put(':id')
     update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
         return this.tasksService.update(Number(id), updateTaskDto);
     }
 
-
-
+    // Only owner can do this
     @Delete(':id')
     delete(@Param('id') id: string) {
         return this.tasksService.delete(Number(id));
