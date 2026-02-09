225b7cf97d42fc5d141ec49c8b197609e615d985
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-09 12:35:07 -0500

    Added basic routing

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { LoginComponent } from './login.component';
+import { DashboardComponent } from './dashboard.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: DashboardComponent },
+];
---
+++ dashboard/src/app/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-login',
+    template: `
+    <div>This is the login page    </div>
+  `,
+})
+export class LoginComponent { }
