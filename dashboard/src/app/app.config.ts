8e0172e5e8a198f98902b518c9ac0678334134bc
Sebastian Hothaza <sebastianhothaza@gmail.com>
2026-02-09 15:19:20 -0500

    added authguard

--- dashboard/src/app/app.config.ts
+++ dashboard/src/app/app.config.ts
@@ -1,10 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { AuthService } from './auth';
+import { firstValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes)
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    provideAppInitializer(() => {
+      const auth = inject(AuthService);
+      return firstValueFrom(auth.checkAuth())
+    })
+
   ]
 };
--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Route } from '@angular/router';
 import { LoginComponent } from './login.component';
 import { DashboardComponent } from './dashboard.component';
+import { AuthGuard } from './auth-guard';
 
 export const appRoutes: Route[] = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'dashboard', component: DashboardComponent },
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
